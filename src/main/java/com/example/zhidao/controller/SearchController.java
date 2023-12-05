a8fbfeecf837a1bcd219b07545fdb62b68050db2
irony_9w <1955502576@qq.com>
2023-12-05 15:36:02 +0800

    修改searchcontroller

--- src/main/java/com/example/zhidao/controller/SearchController.java
+++ src/main/java/com/example/zhidao/controller/SearchController.java
@@ -1,7 +1,10 @@
 package com.example.zhidao.controller;
 
+import com.example.zhidao.dao.ConcernedIssueRepository;
 import com.example.zhidao.mapper.IssueMapper;
+import com.example.zhidao.pojo.entity.ConcernedIssue;
 import com.example.zhidao.pojo.entity.Issue;
+import com.example.zhidao.pojo.entity.User;
 import com.example.zhidao.pojo.vo.common.ResultResponse;
 import com.example.zhidao.pojo.vo.issue.IssueVO;
 import com.example.zhidao.pojo.vo.search.SearchRequest;
@@ -22,16 +25,21 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
     @Autowired
+    private ConcernedIssueRepository concernedIssueRepository;
+    @Autowired
     private UserService userService;
 
     @GetMapping("/search")
     public ResultResponse search(@Valid SearchRequest searchRequest) {
         List<Issue> search = searchService.search(searchRequest.getKeyword(),
                 searchRequest.getPage(), searchRequest.getPageSize());
+        User requestUser = userService.findUserByUsername(searchRequest.getUsername());
         ArrayList<IssueVO> issueVOList = new ArrayList<>();
         for (Issue issue : search) {
+            ConcernedIssue concernedIssue = concernedIssueRepository.findByUserIdAndIssueId(
+                    requestUser.getUserId(), issue.getIssueId());
             issueVOList.add(IssueMapper.INSTANCT.entity2VO(issue, new ArrayList<>(),
-                    userService.findUserInfo(issue.getUserId()).getNickName()));
+                    userService.findUserInfo(issue.getUserId()).getNickName(),concernedIssue!=null));
         }
         return ResultResponse.success(issueVOList);
     }
--- src/main/java/com/example/zhidao/pojo/vo/search/SearchRequest.java
+++ src/main/java/com/example/zhidao/pojo/vo/search/SearchRequest.java
@@ -8,6 +8,8 @@ import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -21,4 +23,8 @@ public class SearchRequest {
     private Integer page;
     @NotNull
     private Integer pageSize;
+    @NotNull
+    @Size(min = 4, max = 20, message = "用户名长度必须在 4-20 之间")
+    @Pattern(regexp = "^[a-zA-Z\\d_]*$", message = "用户名只能包含大小写字母,数字,下划线")
+    private String username;
 }
